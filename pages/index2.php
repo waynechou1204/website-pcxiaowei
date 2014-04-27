bc53ed44e7c7110a3b1f212e31309297c724ce27
waynechou <waynechou1204@gmail.com>
2014-04-27 13:47:50 +0800

    Try to redirect to a remote github repo

---
+++ pages/index2.php
@@ -0,0 +1,293 @@
+<!--
+Author: W3layouts
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>拼车晓位 | Home </title>
+		<link href="web/css/style.css" rel='stylesheet' type='text/css' />
+		
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+		<link rel="shortcut icon" type="image/x-icon" href="web/images/fav-icon.png" />
+		<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+		</script>
+	    <!---strat-slider---->
+	    <script type="text/javascript" src="web/js/jquery.min.js"></script>
+	    <link rel="stylesheet" type="text/css" href="web/css/slider-style.css" />
+		<script type="text/javascript" src="web/js/modernizr.custom.28468.js"></script>
+		<!---//strat-slider---->
+		<!---start-login-script-->
+		<script src="web/js/login.js"></script>
+		<!---//End-login-script-->
+		<!-----768px-menu---->
+		<link type="text/css" rel="stylesheet" href="web/css/jquery.mmenu.all.css" />
+		<script type="text/javascript" src="web/js/jquery.mmenu.js"></script>
+			<script type="text/javascript">
+				//	The menu on the left
+				$(function() {
+					$('nav#menu-left').mmenu();
+				});
+			</script>
+		<!-----//768px-menu---->
+	</head>
+	<body>
+		<!---start-wrap---->
+		<!------start-768px-menu---->
+			<div id="page">
+					<div id="header">
+						<a class="navicon" href="#menu-left"> </a>
+					</div>
+					<nav id="menu-left">
+						<ul>
+							<li><a href="index.html">Home</a></li>
+							<li><a href="about.html">About</a></li>
+							<li><a href="features.html">Features</a></li>
+							<div class="clear"> </div>
+						</ul>
+					</nav>
+			</div>
+		<!------start-768px-menu---->
+			<!---start-header---->
+			<div class="header">
+				<div class="wrap">
+				<div class="header-left">
+					<div class="logo">
+						<a href="index2.php">拼车晓位</a>
+					</div>
+				</div>
+				<div class="header-right">
+					<div class="top-nav">
+					<ul>
+						<li><a href="index.html">Home</a></li>
+						<li><a href="about.html">About</a></li>
+						<li><a href="features.html">联系我们</a></li>
+					</ul>
+				</div>
+				<div class="sign-ligin-btns">
+					<ul>
+						<li id="signupContainer"><a class="signup" id="signupButton" href="#"><span>注册</span></a>
+							 <div class="clear"> </div>
+				                <div id="signupBox">                
+				                    <form id="signupForm">
+				                        <fieldset id="signupbody">
+				                        	<fieldset>
+				                                <label for="username">真实姓名 <span>*</span></label>
+				                                <input type="text" name="username" id="signupusername" />
+				                            </fieldset>
+				                            <fieldset>
+				                                <label for="email">邮箱地址 <span>*</span></label>
+				                                <input type="text" name="email" id="signupemail" />
+				                            </fieldset>
+				                            <fieldset>
+				                                <label for="phone">手机号 <span>*</span></label>
+				                                <input type="text" name="phone" id="signupphone" />
+				                            </fieldset>
+				                            <fieldset>
+				                                <label for="password">请输入密码 <span>*</span></label>
+				                                <input type="password" name="password" id="signuppassword" />
+				                            </fieldset>
+				                            <fieldset>
+				                                <label for="password">确认密码 <span>*</span></label>
+				                                <input type="password" name="password" id="signuppassword1" />
+				                            </fieldset>
+				                            <input type="submit" id="signup" value="立即注册" />
+				                        </fieldset>
+				                    </form>
+				                </div>
+				            <!-- Login Ends Here -->
+						</li>
+						<li id="loginContainer"><a class="login" id="loginButton" href="#"><span>登录</span></i></a>
+							 <div class="clear"> </div>
+				                <div id="loginBox">                
+				                    <form id="loginForm">
+				                        <fieldset id="body">
+				                            <fieldset>
+				                                <label for="email">邮箱地址</label>
+				                                <input type="text" name="email" id="email" />
+				                            </fieldset>
+				                            <fieldset>
+				                                <label for="password">密码</label>
+				                                <input type="password" name="password" id="password" />
+				                            </fieldset>
+				                            <label class="remeber" for="checkbox"><input type="checkbox" id="checkbox" />记住我</label>
+				                            <input type="submit" id="login" value="登录" />
+				                        </fieldset>
+				                        <span><a href="#">忘记密码？</a></span>
+				                    </form>
+				                </div>
+				            <!-- Login Ends Here -->
+						</li>
+						<div class="clear"> </div>
+					</ul>
+				</div>
+				<div class="clear"> </div>
+				</div>
+				<div class="clear"> </div>
+			</div>
+			</div>
+			<!---//End-header---->
+			<!----start-banner---->
+			<div class="text-slider">
+				<div class="wrap"> 
+			<!---start-da-slider---->
+			<div id="da-slider" class="da-slider">
+					<div class="da-slide">
+						<h2>出发！</h2>
+						<p> 风景总在路上</p>
+						<a href="about.html" class="da-link">Find out More</a>
+					</div>
+					<div class="da-slide">
+						<h2>宗旨</h2>
+						<p> Track your Progress Compete with your Friends</p>
+						<a href="about.html" class="da-link">Find out More</a>
+					</div>
+					<div class="da-slide">
+						<h2>通告</h2>
+						<p> Track your Progress Compete with your Friends</p>
+						<a href="about.html" class="da-link">Find out More</a>
+					</div>
+					<div class="da-slide">
+						<h2>t3</h2>
+						<p> Track your Progress Compete with your Friends</p>
+						<a href="about.html" class="da-link">Find out More</a>
+					</div>
+					<div class="da-slide">
+						<h2>t4</h2>
+						<p> Track your Progress Compete with your Friends</p>
+						<a href="about.html" class="da-link">Find out More</a>
+					</div>
+					<nav class="da-arrows">
+						<span class="da-arrows-prev"></span>
+						<span class="da-arrows-next"></span>
+					</nav>
+			</div>
+				<script type="text/javascript" src="web/js/jquery.cslider.js"></script>
+				<script type="text/javascript">
+					$(function() {
+						$('#da-slider').cslider({
+							autoplay	: true,
+							bgincrement	: 450,
+							interval: 10000
+						});
+					
+					});
+				</script>
+			 </div>
+			</div>
+				<!---//End-da-slider----->
+			<!----//End-banner---->
+			<!----start-content-->
+			<div class="content">
+				<div class="wrap">
+					<!--- start-top-grids---->
+					<div class="top-grids">
+						<div class="top-grid">
+							<div class="product-pic">
+								<img src="web/images/pub-search.jpg" title="watch" />
+							</div>
+							<span><label>1</label></span>
+							<div class="border"> </div>
+							<a href="#">发布/搜索拼车信息</a>
+						</div>
+						<div class="top-grid">
+							<div class="product-pic">
+								<img src="web/images/phone.jpg" title="shoe" />
+							</div>
+							<span><label>2</label></span>
+							<div class="border hide"> </div>
+							<a href="#">与拼车友取得联系</a>
+						</div>
+						<div class="top-grid hide">
+							<div class="product-pic">
+								<img src="web/images/sharecar.jpg" title="laptop" />
+							</div>
+							<span><label>3</label></span>
+							<a href="#">按计划出发</a>
+						</div>
+						<div class="clear"> </div>
+					</div>
+					</div>
+					<!--- start-top-grids--->
+					<!---start-mid-grids-->
+					<!--div class="mid-grids">
+						<div class="wrap">
+						<div class="mid-grids-left">
+							<img src="web/images/app-divices.jpg" title="divices" />
+							<span> </span>
+						</div>
+						<div class="mid-grids-right">
+							<h3> Get <span>Runkeeper</span> for your mobile</h3>
+							<p>The Runkeeper app is available for both i<big>OS</big> and <big>Android</big> devices.</p>
+							<ul class="fea">
+								<li><a href="#"><i>Log</i> your runs</a></li>
+								<li><a href="#"><i>Track</i> your Progress </a></li>
+								<li><a href="#">Get a <i>Virtual trainer</i></a></li>
+								<li><a href="#"><i>Complete</i> with your friends </a></li>
+								<li><a href="#"><i>Share</i> your routes</a></li>
+							</ul>
+							<div class="big-btns">
+								<ul>
+									<li><a class="and" href="#"> </a></li>
+									<li><a class="iphone" href="#"> </a></li>
+									<div class="clear"> </div>
+								</ul>
+							</div>
+						</div-->
+						<div class="clear"> </div>
+					</div>
+				</div>
+				<!---//End-mid-grids--->
+				<!---start-bottom-footer-grids---->
+				<div class="footer-grids">
+					<div class="wrap">
+						<div class="footer-grid">
+							<h3>Quick Links</h3>
+							<ul>
+								<li><a href="#">Home</a></li>
+								<li><a href="#">About Features</a></li>
+								<li><a href="#">Login</a></li>
+								<li><a href="#">Sign Up</a></li>
+							</ul>
+						</div>
+						<div class="footer-grid">
+							<h3>More</h3>
+							<ul>
+								<li><a href="#">FAQ</a></li>
+								<li><a href="#">Support</a></li>
+								<li><a href="#">Privacy Policy</a></li>
+								<li><a href="#">Terms and Conditions</a></li>
+							</ul>
+						</div>
+						<div class="footer-grid">
+							<h3>Connect With Us</h3>
+							<ul class="social-icons">
+								<li><a class="facebook" href="#"> </a></li>
+								<li><a class="twitter" href="#"> </a></li>
+								<li><a class="youtube" href="#"> </a></li>
+							</ul>
+							<p class="copy-right">Template by <a href="#">W3layouts</a></p>
+						</div>
+						<div class="footer-grid">
+							<h3>Newsletter</h3>
+							<p>Subscribe to our newsletter to keep up-to-date with all the latest news.</p>
+							<form>
+								<input type="text" class="text" value="Your Name" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Your Name';}">
+								<input type="text" class="text" value="Your Email" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Your Email';}">
+								<input type="submit" value="subscribe" />
+							</form>
+						</div>
+						<div class="clear"> </div>
+					</div>
+				</div>
+				<!---//End-bottom-footer-grids---->
+			</div>
+			<!----//End-content--->
+		<!---//End-wrap---->
+	</body>
+</html>
+
