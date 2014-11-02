98fb123ec80aa7e96320543e4c8ca1f2c59fbcbe
waynechou <waynechou1204@gmail.com>
2014-11-02 12:11:07 +0800

    make footer chinese

--- public_html/footer.php
+++ public_html/footer.php
@@ -4,23 +4,23 @@
 		<div class="footer-grid">
 			<h3>Quick Links</h3>
 			<ul>
-				<li><a href="index.php">Home</a></li>
-				<li><a href="about.php">About Features</a></li>
-				<li><a href="index.php">Login</a></li>
-				<li><a href="index.php">Sign Up</a></li>
+				<li><a href="index.php">首页</a></li>
+				<li><a href="about.php">功能</a></li>
+				<li><a href="index.php">登陆</a></li>
+				<li><a href="index.php">注册</a></li>
 			</ul>
 		</div>
 		<div class="footer-grid">
 			<h3>More</h3>
 			<ul>
-				<li><a href="contact.php">FAQ</a></li>
-				<li><a href="about.php">Support</a></li>
-				<li><a href="about.php">Privacy Policy</a></li>
-				<li><a href="about.php">Terms and Conditions</a></li>
+				<li><a href="contact.php">联系我们</a></li>
+				<li><a href="about.php">技术支持</a></li>
+				<li><a href="about.php">隐私条例</a></li>
+				<li><a href="about.php">法律声明</a></li>
 			</ul>
 		</div>
 		<div class="footer-grid">
-			<h3>Connect With Us</h3>
+			<h3>关注我们</h3>
 			<ul class="social-icons">
 				<li><a class="facebook" href="#"> </a></li>
 				<li><a class="twitter" href="#"> </a></li>
@@ -29,8 +29,8 @@
 			<!--p class="copy-right">Template by <a href="#">W3layouts</a></p-->
 		</div>
 		<div class="footer-grid">
-			<h3>Newsletter</h3>
-			<p>Subscribe to our newsletter to keep up-to-date with all the latest news.</p>
+			<h3>新鲜事</h3>
+			<p>订阅我们以获取最新消息</p>
 			<!--form>
 				<input type="text" class="text" value="Your Name" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Your Name';}">
 				<input type="text" class="text" value="Your Email" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Your Email';}">
